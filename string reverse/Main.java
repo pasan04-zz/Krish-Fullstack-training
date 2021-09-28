652ba3fb4c986753b6baf6463a4572f958fa81a0
pasan04 <pasankamburugamuwa1@gmail.com>
2021-09-28 21:08:40 +0530

    added the bigdecimal java file

--- string reverse/Main.java
+++ string reverse/Main.java
@@ -1,16 +1,42 @@
-
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(reverseString("Pasan Kamburugamuwa"));
+        bigDecimal();
     }
 
-    public static String reverseString(String myString){
-        int len = myString.length();
-        char[] c = new char[len];
-        for(int j= len-1,i=0; j>= 0; j--, i++){
-            c[i] = myString.charAt(j);
-        }
-        return String.copyValueOf(c);
+    public static void bigDecimal(){
+
+        //two new Big Decimals
+        BigDecimal b1 = new BigDecimal("56789123.567892132");
+        BigDecimal b2 = new BigDecimal("12342323.123422323");
+
+
+        //add two decimals
+        BigDecimal addTwoDecimal = b1.add(b2);
+        System.out.println("Add two decimal " + addTwoDecimal);
+
+        //substract two decimals
+        BigDecimal substractTwoDecimal = b1.add(b2);
+        System.out.println("Substract two decimal " + substractTwoDecimal);
+
+        //multiply two decimals
+        BigDecimal multiplyTwoDecimal = b1.multiply(b2);
+        System.out.println("Multiply two decimal " + multiplyTwoDecimal);
+
+        //divide two decimals
+        //When a MathContext object is supplied with a precision setting of 0 (for example, MathContext.UNLIMITED),
+        // arithmetic operations are exact, as are the arithmetic methods which take no MathContext object.
+        BigDecimal divideTwoDecimal = b1.divide(b2, 2, RoundingMode.HALF_UP);
+        System.out.println("Divide two decimal " + divideTwoDecimal);
+
+        // BigDecima1 raised to the power of 2
+        BigDecimal rasideTothePowerDecimalTwo = b1.pow(6);
+        System.out.println("Raise to the power of 2 = " + rasideTothePowerDecimalTwo);
+
+        // Negative value of BigDecimal1
+        BigDecimal negativeValue= b1.negate();
+        System.out.println("Negative value = " + negativeValue);
     }
 }
